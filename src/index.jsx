e7285a2caea5c185497e0528a77675c27d20d648
Macall Smith <89888292+PeachyBuffalo@users.noreply.github.com>
2024-10-25 11:39:53 -0400

    Index with Comp and React

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+// src/index.jsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Component from './Component';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Component />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
